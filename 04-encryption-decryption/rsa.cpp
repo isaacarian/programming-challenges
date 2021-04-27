dc8edd2b2e7815cd7a6e1cba1abe55bc4a2f68bf
Isaac Arian <arianisaac0@gmail.com>
2021-04-27 01:41:12 -0400

    added binary subtraction for 1024 bit sets

--- 04-encryption-decryption/rsa.cpp
+++ 04-encryption-decryption/rsa.cpp
@@ -25,19 +25,75 @@ bool isPrime (bitset<1024> &n)
 		isPrime = false;
 	else
 	{
-		bitset<1024> a = n - 1;
+		bitset<1024> a = n;
 		int counter = 0;
-		while (a[0] == 0)
-		{
-			a / 2;
+		//while (a[0] == 0)
+		//{
 				
-		}		
+		//}		
 	}
 	return isPrime;
 
 }
 
-bitset<1024> bitDivide(bitset<1024> x, bitset<1024, y)
+bitset<1024> binSubtract(bitset<1024> x, bitset<1024> y)
+{ //x-y
+	bitset<1024> difference;
+	for (int i = 0; i < 1024; i++)
+	{
+		if (x[i] == 1 and y[i] == 1)
+			difference[i] = 0;
+		else if (x[i] == 1 and y[i] == 0)
+			difference[i] = 1;
+		else if (x[i] == 0 and y[i] == 0)
+			difference[i] = 0;
+		else if (x[i] == 0 and y[i] == 1)
+		{
+			int counter = 1;
+			while (true)
+			{
+				if (x[i+counter] == 1)
+				{
+					x[i+counter] = 0;
+					difference[i] = 1;
+					break;
+				}
+				else if (x[i+counter] == 0)
+				{
+					x[i+counter] = 1;
+					counter++;
+				}
+			}
+			
+		}
+			
+	}	
+	return difference;
+}
+
+bitset<1024> binDivision(bitset<1024>x)
+{}
+bool binSmaller(bitset<1024> &x, bitset<1024>y)
+{// is x smaller than y
+	bool binSmaller;
+	for (int i = 1023; i >= 0; i--)
+	{
+		if (x[i] == 1 and y[i] == 0)
+		{
+			binSmaller = true;
+			break;
+		}
+		else if (x[i] == 0 and y[i] == 1)
+		{
+			binSmaller = false;
+			break;
+		}
+
+	}
+	
+	return binSmaller;
+}
+bitset<1024> bitDivide(bitset<1024> &x, bitset<1024> &y)
 {
 	bitset<1024> z = 0; 
 }
@@ -48,8 +104,14 @@ int main(int argc, char** argv)
 	srand(time(NULL));
 	string message = argv[1];
 	bitset<1024> n = genBinary();	
+	bitset<1024> b = genBinary();
+	if (binSmaller(n, b))
+	{
+		bitset<1024> d = binSubtract(n,b);
+		cout << d << endl;
+	}
 	cout << n << endl;
-	cout << isPrime(n) << endl;
+	cout << b << endl;
 
 }
 
